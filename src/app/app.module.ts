04ffc2e65920b02913f2519ce1eb3fbe78baf56d
GerardGHub <singian28@gmail.com>
2022-01-03 11:23:55 +0800

    Mean

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// import { MatInputModule } from '@angular/material/input';
+
+
+
+
+
+
+
+
 
 
 @NgModule({
